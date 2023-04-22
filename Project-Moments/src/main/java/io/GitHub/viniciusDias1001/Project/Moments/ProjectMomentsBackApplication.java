d1cba33d7b10d9ab3351e5349489d70a195855c4
Vinicius Dias <105192228+viniciusDias1001@users.noreply.github.com>
2023-04-22 19:55:01 -0300

    Fist commit for Back Moments

---
+++ Project-Moments/src/main/java/io/GitHub/viniciusDias1001/Project/Moments/ProjectMomentsBackApplication.java
@@ -0,0 +1,13 @@
+package io.GitHub.viniciusDias1001.Project.Moments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMomentsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMomentsBackApplication.class, args);
+	}
+
+}
---
+++ Project-Moments/src/test/java/io/GitHub/viniciusDias1001/Project/Moments/ProjectMomentsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.GitHub.viniciusDias1001.Project.Moments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMomentsBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

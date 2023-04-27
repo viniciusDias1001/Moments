7f837114805c89e6d9a2c3a899fe6693cacd3b1d
Vinicius Dias <105192228+viniciusDias1001@users.noreply.github.com>
2023-04-27 00:30:45 -0300

    Read.me

--- src/main/java/io/GitHub/viniciusDias1001/Project/Moments/repository/AlbumRepository.java
+++ src/main/java/io/GitHub/viniciusDias1001/Project/Moments/repository/AlbumRepository.java
@@ -2,8 +2,15 @@ package io.GitHub.viniciusDias1001.Project.Moments.repository;
 
 import io.GitHub.viniciusDias1001.Project.Moments.entities.Album;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AlbumRepository extends JpaRepository<Album,Integer> {
+
+    @Query(value = " select * from cliente c where c.nome like '%:nome%' ", nativeQuery = true)
+    List<Album> encontrarPorAlbum(@Param("nome") String nome );
 }
